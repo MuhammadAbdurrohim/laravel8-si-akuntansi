161a668aca2ba4f6e129f3e61c3d300e30b8d48c
Muhammad Abdurrohim <muhammadabdurrohim574@gmail.com>
2024-05-15 11:12:00 +0700

    first commit

---
+++ resources/views/layouts/footers/nav.blade.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col-md-6">
+        <div class="copyright text-center text-xl-left text-muted">
+            &copy; {{ now()->year }} <a href="" class="font-weight-bold ml-1" target="_blank">Majelis Kodingan</a>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="copyright text-center text-xl-right text-muted">
+            <a target="_blank" href="" class="font-weight-bold ml-1">Sistem Informasi Akuntansi</a> 
+            By <a href="" target="_blank">HimDev</a>
+        </div>
+    </div>
+</div>
